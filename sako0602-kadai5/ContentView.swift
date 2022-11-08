e005cca66c0d5cba010aad2d0603eb1ee8246d39
sasasan03 <sako.hiro62@gmail.com>
2022-11-08 21:06:08 +0900

    Initial Commit

---
+++ sako0602-kadai5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sako0602-kadai5
+//
+//  Created by sako0602 on 2022/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sako0602-kadai5/sako0602_kadai5App.swift
@@ -0,0 +1,17 @@
+//
+//  sako0602_kadai5App.swift
+//  sako0602-kadai5
+//
+//  Created by sako0602 on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct sako0602_kadai5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
