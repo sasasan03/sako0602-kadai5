2e4ad1b2f9546fe82339b645157fd6b1c0614e26
sasasan03 <sako.hiro62@gmail.com>
2022-11-27 16:09:51 +0900

    課題５の提出

--- sako0602-kadai5/ContentView.swift
+++ sako0602-kadai5/ContentView.swift
@@ -3,20 +3,105 @@
 //  sako0602-kadai5
 //
 //  Created by sako0602 on 2022/11/08.
-//
+
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+private enum DivisionError: Error {
+    case leftEmptyError
+    case rightEmptyError
+    case rightZeroError
+    var divisionErrorType: String {
+        switch self {
+        case .leftEmptyError: return "割られる数を入力してください"
+        case .rightEmptyError: return "割る数を入力してください"
+        case .rightZeroError: return "割る数に０を入力しないでください"
         }
-        .padding()
     }
+}
+
+struct ContentView: View {
+    
+    @State private var showingAlert = false
+    @State private var numText1 = ""
+    @State private var numText2 = ""
+    @State private var total:Double = 0
+    @State private var showingLeftEmptyAlert = false
+    @State private var showingRightEmptyAlert = false
+    @State private var showingRightZeroAlert = false
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+                TextField("", text: $numText1)
+                    .frame(width: 50, height: 30)
+                    .border(Color.black)
+                    .padding()
+                Text("÷")
+                TextField("", text: $numText2)
+                    .frame(width: 50, height: 30)
+                    .border(Color.black)
+                    .padding()
+            }
+            Button("計算") {
+                do {
+                    try total = calc()
+                } catch let error as DivisionError {
+                    showingAlert = true
+                    switch error {
+                    case .leftEmptyError:
+                        showingLeftEmptyAlert = true
+                    case .rightEmptyError:
+                        showingRightEmptyAlert = true
+                    case .rightZeroError:
+                        showingRightZeroAlert = true
+                    }
+                } catch {
+                    print("不明なエラー: \(error)")
+                }
+            }
+            .alert("課題５", isPresented: $showingAlert) {
+            } message: {
+                let divisionErrorLeft = DivisionError.leftEmptyError
+                let alertLeftEmpty = divisionErrorLeft.divisionErrorType
+                let divisionErrorRightEmpty = DivisionError.rightEmptyError
+                let alertRightEmpty = divisionErrorRightEmpty.divisionErrorType
+                let divisionErrorRightZero = DivisionError.rightZeroError
+                let alertRightZero = divisionErrorRightZero.divisionErrorType
+                if showingLeftEmptyAlert {
+                    Text(alertLeftEmpty)
+                }
+                if showingRightEmptyAlert {
+                    Text(alertRightEmpty)
+                }
+                if showingRightZeroAlert {
+                    Text(alertRightZero)
+                }
+            }
+            Text(String(format: "%.2f", total))
+                .padding()
+        }
+    }
+    
+    func calc () throws -> Double {
+        //Optional型のnum1とnum2
+        //アンラップしなければならない。guardでアンラップ
+        let num1 = Double(numText1)
+        let num2 = Double(numText2)
+        
+        guard let unwrappedNum1 = num1 else {
+            throw DivisionError.leftEmptyError
+        }
+        guard let unwrappedNum2 = num2 else {
+            throw DivisionError.rightEmptyError
+        }
+        guard num2 != 0 else {
+            throw DivisionError.rightZeroError
+        }
+        return unwrappedNum1 / unwrappedNum2
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- sako0602-kadai5/MyPlayground.playground/Contents.swift
+++ sako0602-kadai5/MyPlayground.playground/Contents.swift
@@ -1,3 +1,87 @@
-import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+var alert = false
+
+
+func calc(numText1: String, numText2: String) throws -> Double {
+    let num1 = Double(numText1)
+    let num2 = Double(numText2)
+    
+    guard let unwrappedNum1 = num1 else {
+        alert = true
+        print("num1は無効な数です")
+        
+        throw DivisionError.leftEmptyError
+    }
+    guard let unwrappedNum2 = num2 else {
+        alert = true
+        print("num2は無効な数です")
+        throw DivisionError.rightEmptyError
+    }
+    
+    guard num2 != 0 else {
+        alert = true
+        print("num2は0です")
+        throw DivisionError.rightZeroError
+    }
+    print("すべてのguardを通りました")
+    
+    return unwrappedNum1 / unwrappedNum2
+}
+
+enum DivisionError: Error {
+    case leftEmptyError
+    case rightEmptyError
+    case rightZeroError
+    
+    var divisionErrorType: String {
+        switch self {
+        case .leftEmptyError: return "左が空"
+        case .rightEmptyError: return "右が空"
+        case .rightZeroError: return "右が０"
+        }
+    }
+}
+
+var a = false
+var b = false
+var c = false
+
+do {
+    var total:Double = 0
+    try total = calc(numText1: "9", numText2: "")
+    print("計算結果は\(total)です")
+    //キャストする意味とは？
+    //error -> opt + クリック（型が見れる）-> error はErrorプロトコルになっている。 -> errorをDivisionErrorとして扱うためにキャストを行う。
+} catch let error as DivisionError {
+    //@State var alertText = ""をstructのスコープ内で宣言すると、書き換え易いためよくない。
+    // ❌ alertText = error.divisionErrorType
+    //ケースに分けて書く
+    switch error {
+    case .leftEmptyError:
+        a = true
+    case .rightEmptyError:
+        b = true
+    case .rightZeroError:
+        c = true
+    }
+    print(error.divisionErrorType)
+}
+
+let divisionError = DivisionError.leftEmptyError
+let leftEmpty = divisionError.divisionErrorType
+let divisionErrorRight = DivisionError.rightEmptyError
+let rightEmpty = divisionErrorRight.divisionErrorType
+
+if a {
+    print(leftEmpty)
+}
+if b {
+    print(rightEmpty)
+}
+if c {
+    print("c")
+}
+
+
+
